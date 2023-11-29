229baaaff07fba3803a58bd8d0e9bc10b8bc5c95
Giovanni Longo <giovanni.longo10@icloud.com>
2023-11-29 17:19:31 +0100

    add new user

--- src/ESERCIZIO3/Main.java
+++ src/ESERCIZIO3/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Utente utente = new Utente(1, "Giovanni", "Longo", "Giovanni@email.com", LocalDate.now());
         Utente utente2 = new Utente(2, "Marco", "Polo", "Marco@email.com", LocalDate.now());
-
+        Utente utente3 = new Utente(3, "Giulio", "Cesare", "Giulio@email.com", LocalDate.now());
 
         Articolo articolo1 = new Articolo(92199, "Prodotto 1", 20, 10);
         Articolo articolo2 = new Articolo(23222, "Prodotto 2", 30, 5);
@@ -14,12 +14,15 @@ public class Main {
 
         Carrello carrelloUtente = new Carrello(utente.getCodiceCliente());
         Carrello carrelloUtente2 = new Carrello(utente2.getCodiceCliente());
+        Carrello carrelloUtente3 = new Carrello(utente3.getCodiceCliente());
 
         utente.aggiungiArticoloAlCarrello(articolo1, carrelloUtente);
         utente.aggiungiArticoloAlCarrello(articolo2, carrelloUtente);
         utente2.aggiungiArticoloAlCarrello(articolo2, carrelloUtente2);
+        utente3.aggiungiArticoloAlCarrello(articolo1, carrelloUtente3);
 
         carrelloUtente.visualizzaCarrello();
         carrelloUtente2.visualizzaCarrello();
+        carrelloUtente3.visualizzaCarrello();
     }
 }
